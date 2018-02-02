4ef084dbf16bc9e52d62425fd0f818d4f239c311
Loy <puregithub@yeah.net>
2018-02-02 12:18:58 +0800

    release 0.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'exp-kit',
     packages = find_packages(exclude=('config')),
-    version = '0.0.6',
+    version = '0.0.7',
     description = 'A CLI tool facilitating experimentation on AWS.',
     author = 'All-less',
     author_email = 'all.less.mail@gmail.com',
